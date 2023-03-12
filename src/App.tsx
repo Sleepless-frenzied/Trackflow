39bb912f4cd3e7b0ce61b22385a5c05711b33c56
Xyphes <willy.somkhit@epita.fr>
2023-03-12 22:55:56 +0100

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+
+
+
+import { Routes, Route} from "react-router-dom";
+//import Navbar from "./Components/Navbar";
+import Home from "./Pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-      </header>
-    </div>
+      <div className="App">
+
+          {/*<header className="fixed w-full h-full z-50">
+          <Navbar/>
+        </header>*/}
+
+          <p>tt</p>
+
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+        </Routes>
+
+
+      </div>
   );
 }
 
